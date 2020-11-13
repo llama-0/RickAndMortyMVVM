fb590be57060d379f6ef79f6f076a09c978b8d70
llama-0 <anrozan93@mail.ru>
2020-11-13 15:19:09 +0300

    simplified `no internet connection` screen

--- app/src/main/java/com/llama/rick_and_morty_mvvm/ui/view/HomeFragment.kt
+++ app/src/main/java/com/llama/rick_and_morty_mvvm/ui/view/HomeFragment.kt
@@ -1,8 +1,5 @@
 package com.llama.rick_and_morty_mvvm.ui.view
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkInfo
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -10,11 +7,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.snackbar.Snackbar
 import com.llama.rick_and_morty_mvvm.R
-import com.llama.rick_and_morty_mvvm.ui.viewmodel.HomeViewModelFactory
 import com.llama.rick_and_morty_mvvm.data.RepositoryImpl
 import com.llama.rick_and_morty_mvvm.data.model.SimpleCharacter
-import com.llama.rick_and_morty_mvvm.data.network.Resource
 import com.llama.rick_and_morty_mvvm.ui.viewmodel.HomeViewModel
+import com.llama.rick_and_morty_mvvm.ui.viewmodel.HomeViewModelFactory
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.progressbar_layout.*
 import kotlinx.android.synthetic.main.recycler_error_layout.*
@@ -36,8 +32,6 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     }
 
     private fun refreshRecyclerViewData() {
-//        progress_bar_layout.visibility = View.GONE
-//        recycler_error_layout.visibility = View.GONE
         viewModel.getLiveDataList().observe(viewLifecycleOwner, {
             when {
                 it.isNotEmpty() -> initAdapter(it)
@@ -48,6 +42,8 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private fun initAdapter(list: List<SimpleCharacter>) {
         Log.d(this@HomeFragment.toString(), "initAdapter: called")
+        progress_bar_layout.visibility = View.GONE
+        recycler_error_layout.visibility = View.GONE
         rv_items.adapter = HomeAdapter(list) { character ->
             showSnackbar(rv_items, character.name)
         }
@@ -56,28 +52,6 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private fun showErrorLayout() {
         Log.d(this@HomeFragment.toString(), "showErrorScreen: error")
         recycler_error_layout.visibility = View.VISIBLE
-        btn_retry.setOnClickListener {
-            if (isNetworkConnected()) {
-                showLoading()
-                recycler_error_layout.visibility = View.GONE // something funny is happening here...
-                progress_bar_layout.visibility = View.GONE
-                refreshRecyclerViewData()
-            } else {
-                showSnackbar(fragment_home_layout, getString(R.string.check_internet_connection_message))
-            }
-        }
-    }
-
-    private fun showLoading() {
-        progress_bar_layout.visibility = View.VISIBLE
-        Log.d(this@HomeFragment.toString(), "showLoading: progress bar")
-    }
-
-    // todo: rewrite this logic without deprecation or use something else instead
-    private fun isNetworkConnected(): Boolean {
-        val cm = activity?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
-        return activeNetwork?.isConnectedOrConnecting == true
     }
 
     private fun showSnackbar(view: View, message: String) {
--- app/src/main/java/com/llama/rick_and_morty_mvvm/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/llama/rick_and_morty_mvvm/ui/viewmodel/HomeViewModel.kt
@@ -3,18 +3,16 @@ package com.llama.rick_and_morty_mvvm.ui.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
 import com.llama.rick_and_morty_mvvm.data.RepositoryImpl
 import com.llama.rick_and_morty_mvvm.data.model.SimpleCharacter
 import com.llama.rick_and_morty_mvvm.data.network.Resource
-import com.llama.rick_and_morty_mvvm.ui.view.HomeFragment
 
-class HomeViewModel(repository: RepositoryImpl) : ViewModel() {
+class HomeViewModel(private val repository: RepositoryImpl) : ViewModel() {
 
     private val liveDataList: MutableLiveData<List<SimpleCharacter>> = MutableLiveData()
 
     fun getLiveDataList(): LiveData<List<SimpleCharacter>> {
-        RepositoryImpl().getCharacters(object : Resource{ // repository which has been passed to HomeViewModel constructor is unreachable here (why?)
+        repository.getCharacters(object : Resource{
             override fun onSuccess(data: List<SimpleCharacter>) {
                 liveDataList.value = data
             }
