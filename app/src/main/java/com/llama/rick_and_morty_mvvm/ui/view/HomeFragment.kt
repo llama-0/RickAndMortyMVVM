06aac7cda54206c5a74803bf647d7e5dd6443cd5
llama-0 <anrozan93@mail.ru>
2020-11-19 15:12:34 +0300

    restored bits of functionality

--- app/src/main/java/com/llama/rick_and_morty_mvvm/ui/view/HomeFragment.kt
+++ app/src/main/java/com/llama/rick_and_morty_mvvm/ui/view/HomeFragment.kt
@@ -1,8 +1,11 @@
 package com.llama.rick_and_morty_mvvm.ui.view
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.core.view.isEmpty
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.snackbar.Snackbar
@@ -22,6 +25,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         super.onViewCreated(view, savedInstanceState)
 
         initViewModel()
+        initRetryButton()
         refreshRecyclerViewData()
     }
 
@@ -33,66 +37,27 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         }
     }
 
-    /*
-    * 1. как работает лайвдата?
-    * 2. почему setUpNormalView иногда со второго раза появляется?
-    * 3. почему progressbar постоянно Visible до тех пор, пока не загрузятся данные,
-    *   если зайти в приложение с изначально выключенным интернетом, хотя по логам
-    *   isLoading == false !?!
-    * Бонусы: 1. вернулась к состоянию лишних запросов при повороте экрана
-    *         2. по кнопке retry без интернета больше не показываю снекбар
-    *         3. всё ещё есть логика во фрагменте, а надо убрать
-    *         4. общий класс не создан
-    * */
+    private fun initRetryButton() {
+        btn_retry.setOnClickListener {
+            viewModel.retryBtn.observe(viewLifecycleOwner, {
+                viewModel.snackbarMessage.observe(viewLifecycleOwner, {
+                    showSnackbar(fragment_home_layout, getString(R.string.check_internet_connection_message))
+                })
+            })
+        }
+    }
+
     private fun refreshRecyclerViewData() {
-        viewModel.dataList.observe(viewLifecycleOwner, {
-            initAdapter(it)
-            Log.d(TAG, "refreshRecyclerViewData: initing daapter for the `first` time")
-        })
-        viewModel.errorState.observe(viewLifecycleOwner, {
-            showErrorLayout()
-        })
-        viewModel.loadState.observe(viewLifecycleOwner, {
-            progress_bar_layout.visibility = it
-        })
+        viewModel.dataList.observe(viewLifecycleOwner, { initAdapter(it) })
+        viewModel.errorState.observe(viewLifecycleOwner, { recycler_error_layout.isVisible = it })
+        viewModel.loadState.observe(viewLifecycleOwner, { progress_bar_layout.isVisible = it })
     }
 
     // через vm вызов снекбара сделать.
     private fun initAdapter(list: List<SimpleCharacter>) {
-        setUpNormalView()
         rv_items.adapter = HomeAdapter(list) { character ->
-            showSnackbar(rv_items, character.name)
+            showSnackbar(rv_items, character.name) // viewModel.onItemClicked() ?
         }
-    }
-
-    // getCharacters явно вызывать. на onError менять value
-
-    private fun showErrorLayout() {
-        Log.d("TAG", "showErrorLayout: inside")
-        setUpErrorView()
-        btn_retry.setOnClickListener {
-//            viewModel.onButtonClicked()
-//                    .observe(viewLifecycleOwner, {
-//                showSnackbar(fragment_home_layout, getString(R.string.check_internet_connection_message))
-//            })
-//            viewModel.onButtonClickedDone()
-            viewModel.dataList.observe(viewLifecycleOwner, {
-                Log.d(TAG, "refreshRecyclerViewData: initing adapter for the `second` time")
-                initAdapter(it)
-            })
-            // how to intercept connection timeout ?
-        }
-    }
-
-    private fun setUpNormalView() {
-        recycler_error_layout.visibility = View.GONE
-//        progress_bar_layout.visibility = View.GONE
-        rv_items.visibility = View.VISIBLE
-    }
-
-    private fun setUpErrorView() {
-        recycler_error_layout.visibility = View.VISIBLE
-        rv_items.visibility = View.GONE
     }
 
     private fun showSnackbar(view: View, message: String) {
--- app/src/main/java/com/llama/rick_and_morty_mvvm/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/llama/rick_and_morty_mvvm/ui/viewmodel/HomeViewModel.kt
@@ -1,7 +1,6 @@
 package com.llama.rick_and_morty_mvvm.ui.viewmodel
 
 import android.util.Log
-import android.view.View // is this The reference to a View I shouldn't be including in ViewModel ??
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -12,53 +11,68 @@ import com.llama.rick_and_morty_mvvm.domain.model.SimpleCharacter
 
 class HomeViewModel(private val repository: RepositoryImpl) : ViewModel() {
 
-//    private val snackbarAction = ActionLiveData<Boolean>()
-//    private val isButtonClicked = MutableLiveData<Boolean>()
-
     private val liveDataList = MutableLiveData<List<SimpleCharacter>>()
-    private val isErrorPresent = MutableLiveData<Boolean>()
-    private val isLoading = MutableLiveData<Int>()
+    private val recyclerViewVisibility = MutableLiveData<Boolean>() // normal view state,
+                                                                    // may be redundant as errorLayout overlaps rv ...
+
+    private val errorLayoutVisibility = MutableLiveData<Boolean>() // errorLayoutVisibility
+    private val progressBarVisibility = MutableLiveData<Boolean>() // progressBarVisibility
+
+    private val snackbarAction = MutableLiveData<Boolean>()
+    private val isRetryButtonClicked = MutableLiveData<Boolean>()
 
     val errorState: LiveData<Boolean>
-        get() = isErrorPresent
-    val loadState: LiveData<Int>
-        get() = isLoading
+        get() = errorLayoutVisibility
+    val loadState: LiveData<Boolean>
+        get() = progressBarVisibility
     val dataList: LiveData<List<SimpleCharacter>>
         get() {
             loadCharacters()
             return liveDataList
         }
+    val retryBtn: LiveData<Boolean>
+        get() {
+            loadCharacters()
+            isRetryButtonClicked.value = true
+            return isRetryButtonClicked
+        }
+    val snackbarMessage: LiveData<Boolean>
+        get() = snackbarAction
 
     private fun loadCharacters() {
-        isLoading.value = View.VISIBLE
+        progressBarVisibility.value = true
         repository.getCharacters(object : Resource {
             override fun onSuccess(data: List<SimpleCharacter>) {
+                setSuccessState()
                 liveDataList.value = data
-                isLoading.value = View.GONE
             }
 
             override fun onError() {
-                isErrorPresent.value = true
-                isLoading.value = View.GONE
-                Log.d(TAG, "onError: loading should be set to GONE = ${isLoading.value}")
+                setErrorState()
+                Log.d(TAG, "onError: loading should be set to false = ${progressBarVisibility.value}")
             }
 
         })
     }
 
-//    fun updateErrorState(): LiveData<Boolean> {
-//        isErrorPresent.value = false
-//        return isErrorPresent
-//    }
+    fun setErrorState() {
+        recyclerViewVisibility.value = false
+        errorLayoutVisibility.value = true
+        progressBarVisibility.value = false
+        if (isRetryButtonClicked.value == true)
+            snackbarAction.value = true
+    }
 
-//    fun updateUI(): LiveData<List<SimpleCharacter>> {
-//        if (liveDataList.value.isNullOrEmpty()) { //
-//            loadCharacters()
-//        }
-//        return liveDataList
-//    }
+    fun setSuccessState() {
+        recyclerViewVisibility.value = true
+        progressBarVisibility.value = false
+        errorLayoutVisibility.value = false
+        isRetryButtonClicked.value = false
+    }
 
     companion object {
         private const val TAG = "TAG"
     }
 }
+
+class SnackbarMessage(val text: String)
