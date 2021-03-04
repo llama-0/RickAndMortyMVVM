19a86c64846892c3b66b4e087813db3c20315bd7
llama-0 <anrozan93@mail.ru>
2021-03-04 16:08:55 +0300

    dirty rxjava works

--- app/src/main/java/com/llama/rick_and_morty_mvvm/domain/interactor/CharactersInteractor.kt
+++ app/src/main/java/com/llama/rick_and_morty_mvvm/domain/interactor/CharactersInteractor.kt
@@ -3,13 +3,14 @@ package com.llama.rick_and_morty_mvvm.domain.interactor
 import com.llama.rick_and_morty_mvvm.data.Repository
 import com.llama.rick_and_morty_mvvm.domain.model.SimpleCharacter
 import io.reactivex.rxjava3.core.Single
-
 class CharactersInteractor(private val repository: Repository) {
 
     private val cachedData: ArrayList<SimpleCharacter> = arrayListOf()
 
-    fun fetchData(): Single<List<SimpleCharacter>> =
-        repository.getCharacters()
+    fun fetchData(): Single<List<SimpleCharacter>> {
+        return repository.getCharacters()
+            .doOnSuccess { cachedData.addAll(it) } // bad, try with flatMap or something
+    }
 
     fun getCachedData(): List<SimpleCharacter> =
         cachedData
@@ -19,3 +20,4 @@ class CharactersInteractor(private val repository: Repository) {
         private const val TAG = "CharactersInteractor"
     }
 }
+
--- app/src/main/java/com/llama/rick_and_morty_mvvm/ui/viewmodel/CharactersViewModel.kt
+++ app/src/main/java/com/llama/rick_and_morty_mvvm/ui/viewmodel/CharactersViewModel.kt
@@ -14,6 +14,8 @@ import com.llama.rick_and_morty_mvvm.ui.model.GenderFilter
 import com.llama.rick_and_morty_mvvm.ui.model.GenderType
 import com.llama.rick_and_morty_mvvm.ui.view.screenstate.CharactersScreenState
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.observers.DisposableSingleObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class CharactersViewModel(
@@ -27,6 +29,8 @@ class CharactersViewModel(
 
     private var list: List<SimpleCharacter>? = null
     private var chipsIdsToGenderTypes: List<Pair<Int, GenderType>>? = null
+
+    private var disposable: Disposable? = null
 
     init {
         loadCharacters()
@@ -59,33 +63,48 @@ class CharactersViewModel(
 
     private fun loadCharacters() {
         updateScreenState(progressBarVisibilityState = true)
-        interactor.fetchData()
-            .observeOn(Schedulers.io())
-            .subscribeOn(AndroidSchedulers.mainThread())
-            .doOnSuccess {
-                list = it
-                Log.e(TAG, "Success ----------------- list size = ${it.size}")
-                updateScreenState(
-                    dataListState = it,
-                    errorLayoutVisibilityState = false,
-                    progressBarVisibilityState = false,
-                    chipsGroupVisibilityState = true,
-                    isBtnRetryClicked = false
-                )
-            }
-            .doOnError {
-                Log.e(TAG, "onError: getCharacters from remote failed", it)
-                if (screenState.isBtnRetryClicked) {
-                    executeCommand(ShowSnackbar(resources.getString(R.string.check_internet_connection_message)))
+        disposable = interactor.fetchData()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(object : DisposableSingleObserver<List<SimpleCharacter>>() {
+                override fun onSuccess(t: List<SimpleCharacter>?) {
+                    Log.e(TAG, "Success ----------------- list size = ${t?.size}")
+                    list = t
+                    updateScreenState(
+                        dataListState = t ?: return,
+                        errorLayoutVisibilityState = false,
+                        progressBarVisibilityState = false,
+                        chipsGroupVisibilityState = true,
+                        isBtnRetryClicked = false
+                    )
+
                 }
-                updateScreenState(
-                    errorLayoutVisibilityState = true,
-                    progressBarVisibilityState = false,
-                    chipsGroupVisibilityState = false
-                )
-            }
+
+                override fun onError(e: Throwable?) {
+                    Log.e(TAG, "onError: getCharacters from remote failed", e)
+                    if (screenState.isBtnRetryClicked) {
+                        executeCommand(ShowSnackbar(resources.getString(R.string.check_internet_connection_message)))
+                    }
+                    updateScreenState(
+                        errorLayoutVisibilityState = true,
+                        progressBarVisibilityState = false,
+                        chipsGroupVisibilityState = false
+                    )
+                }
+
+            })
     }
 
+    // todo: probably need to move disposable to BaseVM and make it `protected` and onCleared() method also move there
+    override fun onCleared() {
+        super.onCleared()
+        if (disposable != null) {
+            disposable?.dispose()
+            disposable = null
+        }
+    }
+
+    @Suppress("unused")
     private fun updateCheckedGenderTypes(genderType: GenderType) {
         // как в задче со скобочками. Если среди screenState.checkedGenderTypes уже есть тип == genderType,
         // значит мы второй раз нажимаем на чипсину, значит мы должны отселектить её (и удалить из списка)
